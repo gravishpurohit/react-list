1edb8d5b0b318b9e4f3f34bb65e67445b8e81c7f
Gravish Purohit <gravishpurohit@Gravishs-MacBook-Air.local>
2021-05-16 18:31:46 +0530

    feat: add application skeleton
    Signed-off-by: Gravish Purohit <gravishpurohit@Gravishs-MacBook-Air.local>

--- src/App.js
+++ src/App.js
@@ -1,25 +1,81 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import Listitems from "./component/Listitems";
+import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+Array.prototype.remove = function () {
+  var what,
+    a = arguments,
+    L = a.length,
+    ax;
+  while (L && this.length) {
+    what = a[--L];
+    while ((ax = this.indexOf(what)) !== -1) {
+      this.splice(ax, 1);
+    }
+  }
+  return this;
+};
+
+var listObjects = [];
+
+class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      items: [],
+      currentItem: {
+        text: "",
+      },
+    };
+    this.handleInput = this.handleInput.bind(this);
+    this.addItem = this.addItem.bind(this);
+  }
+  handleInput(event) {
+    this.setState({
+      currentItem: {
+        text: event.target.value,
+      },
+    });
+  }
+  addItem(event) {
+    event.preventDefault();
+    const newItem = this.state.currentItem;
+    if (listObjects.includes(newItem.text)) {
+      listObjects.remove(newItem.text);
+    }
+    listObjects.unshift(newItem.text);
+    console.log(listObjects);
+    if (newItem.text !== "") {
+      const newItems = [...this.state.items, newItem];
+      this.setState({
+        items: newItems,
+        currentItem: {
+          text: "",
+        },
+      });
+    }
+  }
+  render() {
+    return (
+      <div>
+        <div className="fan">
+          <h1>List Of Items</h1>
+        </div>
+        <div className="app">
+          <form id="list" onSubmit={this.addItem}>
+            <input
+              type="text"
+              placeholder="Enter Text"
+              value={this.state.currentItem.text}
+              onChange={this.handleInput}
+            />
+            <button type="submit">click</button>
+          </form>
+          <Listitems items={listObjects}></Listitems>
+        </div>
+      </div>
+    );
+  }
 }
 
 export default App;
